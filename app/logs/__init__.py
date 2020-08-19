51910e38c74a0fb34b268bbf4495e343fa373394
krau5 <apvbd17@gmail.com>
2020-08-19 11:07:53 +0300

    logger

---
+++ app/logs/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(
+    handlers=[logging.FileHandler('logs/app.log', 'a', 'utf-8')],
+    format='%(asctime)%- %(levelname)% - %(message)%',
+    datefmt='%d-%m-%y %H:%M',
+    level=logging.INFO
+)
+logger = logging.getLogger('app')
